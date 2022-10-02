ecacddfca282e9ea877388b4ed04825956fb3948
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-10-02 16:58:31 +0200

    php updates

--- php/hotel.php
+++ php/hotel.php
@@ -28,7 +28,61 @@ if(isset($_POST['compare'])){
 
 }
 
+class hotel{
 
+    //properties
+
+    private $hotelName;
+    private $roomRate;
+    private $offerRate;
+
+    // setter method
+
+    public function comparePrices(string $hotelName, int $roomRate, int $offerRate) {
+        $this->hotelName = $hotelName;
+        $this->roomRate = $roomRate;
+        $this->offerPrice = $offerPrice;
+        
+    }
+
+    //getter method
+
+    public function getCheapest(){
+
+    }
+}
+
+$hotel_1 = new hotel();
+$hotel_1 -> comparePrices("City Lodge Hotel, Bloemfontein",600,30);
+$hotel_1 -> getCheapest();
+
+$hotel_2 = new hotel();
+$hotel_2 -> comparePrices("Game View Lodge, Vryburg",400,400);
+$hotel_2 -> getCheapest();
+
+$hotel_3 = new hotel();
+$hotel_3 -> comparePrices("Gateway Hotel, Durban",950,95);
+$hotel_3 -> getCheapest();
+
+$hotel_4 = new hotel();
+$hotel_4 -> comparePrices("Ocean Breeze Hotel, Strand",460,460);
+$hotel_4 -> getCheapest();
+
+$hotel_5 = new hotel();
+$hotel_5 -> comparePrices("Protea by Marriott Hotel, Kimberley",630,32);
+$hotel_5 -> getCheapest();
+
+$hotel_6 = new hotel();
+$hotel_6 -> comparePrices("Radisson Blu Hotel, Port Elizabeth",600,90);
+$hotel_6 -> getCheapest();
+
+$hotel_7 = new hotel();
+$hotel_7 -> comparePrices("Red Sand Country Lodge, Kalahari",865,86,5);
+$hotel_7 -> getCheapest();
+
+$hotel_8 = new hotel();
+$hotel_8 -> comparePrices("Umlilo Lodge, St. Lucia",1550,232,5);
+$hotel_8 -> getCheapest();
 
 
 
@@ -38,7 +92,7 @@ $hotel = array(
     $hotel_1 => array(
         "name" => "Ocean Breeze Hotel",
         "roomRate" => 460,
-        "discount" => 5,
+        "discount" => 0,
         "offerPrice" => function(){
             $offerRate = this.roomRate * ((100 - this.discount)/100);
             return $offerRate;
